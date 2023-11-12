1466bd16751ce6be99d02cde33a76b6c02f5f921
SvetlanaRomanova1 <sromanova@team.amocrm.com>
2023-11-12 14:32:14 +0300

    fix comments

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { celebrate, Joi } = require('celebrate');
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
 const {
   getCards,
   createCard,
@@ -15,30 +18,45 @@ const {
 router.get('/', getCards);
 // Роутер для создания карточки
 router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
-  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      link: Joi.string()
+        .required()
+        .regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    }),
 }), createCard);
 // Роутер для удаления карточки по идентификатору
 router.delete('/:cardId', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .hex()
+        .length(24),
+    }),
 }), deleteCardId);
 // Роутер для лайка карточке
 router.put('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .hex()
+        .length(24),
+    }),
 }), likeCard);
 // Роутер удаления карточки по идентификатору
 router.delete(
   '/:cardId/likes',
   celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
+    params: Joi.object()
+      .keys({
+        cardId: Joi.string()
+          .hex()
+          .length(24),
+      }),
   }),
   dislikeCard,
 );
